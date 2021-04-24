1f4625a2a7aab6f030e9095d3eff8bb0728bb67e
Vikrant Dwivedi <vkrntdwivedi224@gmail.com>
2021-04-24 11:19:35 -0400

    Added Core Data stack

---
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -0,0 +1,8 @@
+//
+//  ManagedCache.swift
+//  EssentialFeed
+//
+//  Created by Vikant on 2021-04-24.
+//
+
+import Foundation
---
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -0,0 +1,8 @@
+//
+//  ManagedFeedImage.swift
+//  EssentialFeed
+//
+//  Created by Vikant on 2021-04-24.
+//
+
+import Foundation
